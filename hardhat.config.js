c696b90e90fbf1f8423fce63817a47d1fcb83239
Boris1991 <alekseev.ba22@gmail.com>
2022-01-10 13:21:12 +0300

    https://github.com/Boris1991/hardhat-project

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,10 @@
+
+
 require("@nomiclabs/hardhat-waffle");
 
 // To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-
+const UNFURA_URL = 'https://rinkeby.infura.io/v3/561015c93ab047d792ce1f1208d0c43d'
 
 module.exports = {
   solidity: "0.8.4",
